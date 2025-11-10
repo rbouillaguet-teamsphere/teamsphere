d5ffd928ca0b6c323d78615129a85d1e0654c084
rbouillaguet-teamsphere <238002349+rbouillaguet-teamsphere@users.noreply.github.com>
2025-11-10 11:47:30 +0100

    Activation iOS

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -8,6 +8,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        CAPLog.setLevel(CAPLogLevel.debug)
         return true
     }
 
